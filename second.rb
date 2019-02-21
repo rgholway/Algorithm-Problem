e180f7ae495991eb6ade0de176ee68d67cc99a7e
Richard Holway <rgholway@syr.edu>
2019-02-21 17:24:51 -0500

    second attempt

---
+++ second.rb
@@ -0,0 +1,17 @@
+b = [1, 3, 5, 4, 513, 7, 2, 1, 8]
+array = b.sort
+puts array
+i = 0
+numbers = Hash.new
+20.times do
+  i += 1
+  numbers[i] = 0
+end
+array.each {|item|
+if numbers[item] = item
+  numbers[item] = 1
+end
+}
+
+selected = numbers.key(0)
+puts selected
